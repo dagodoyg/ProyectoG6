bd634c72e21d66ae375c355f7ac0f466cd7a8306
davidgggamerYT <davidgodoygarzon@gmail.com>
2025-07-11 14:13:33 -0500

    working digit identifier

---
+++ dnnReader/main.cpp
@@ -0,0 +1,32 @@
+#include "functions.h"
+#include <dlib/dnn.h>
+#include <dlib/data_io.h>
+#include <iostream>
+
+int main(int argc, char** argv){
+    std::string ImgPath = argv[1];
+    std::vector<Shape> Figs;
+
+    crop_nums<28>(ImgPath, Figs);
+
+    using net_type = dlib::loss_multiclass_log<
+                                dlib::fc<10,        
+                                dlib::relu<dlib::fc<84,   
+                                dlib::relu<dlib::fc<120,  
+                                dlib::max_pool<2,2,2,2,dlib::relu<dlib::con<16,5,5,1,1,
+                                dlib::max_pool<2,2,2,2,dlib::relu<dlib::con<6,5,5,1,1,
+                                dlib::input<dlib::matrix<unsigned char>> 
+                                >>>>>>>>>>>>;
+    net_type net;
+
+    dlib::deserialize("mnist_network.dat") >> net;
+    dlib::image_window win;
+    win.set_image(Figs[0].digit);
+    win.wait_until_closed();
+    std::cout << Figs.size() << std::endl;
+    int x = net(Figs[0].digit); 
+    std::cout << "Clase predicha: " << x << std::endl;
+    
+
+    return 0;
+}
