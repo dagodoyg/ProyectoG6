8eb1213fd73b57cd2fce9e1341dc65b55f9bdf0d
davidgggamerYT <davidgodoygarzon@gmail.com>
2025-07-11 21:34:58 -0500

    noise generator

--- NoiseCandela/noise_candela.cpp
+++ NoiseCandela/noise_candela.cpp
@@ -11,12 +11,9 @@
 template <typename image_t>
 void svm_format(cv::Mat & cv_digit, image_t & dlib_digit, const int & D_SIZE){
     cv::resize(cv_digit,cv_digit,cv::Size(D_SIZE,D_SIZE));
-    cv::bitwise_not(cv_digit, cv_digit); //Black letter over white background
-    //cv::transpose(cv_digit,cv_digit);  //Transpose, cv::Mat is row major, dlib::matrix is column major
+    cv::bitwise_not(cv_digit, cv_digit);
     cv_digit.convertTo(cv_digit, CV_8UC1); 
-
     if (!cv_digit.isContinuous()) cv_digit = cv_digit.clone() ; //Force contiguity
-
     dlib::assign_image(dlib_digit, dlib::cv_image<unsigned char>(cv_digit));
 }
 
